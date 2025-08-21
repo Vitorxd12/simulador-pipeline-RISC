b1ae838bc317f4558dad8dd71b56a7226e2b9187
Vitor Costa <Vitorxd12.12@gmail.com>
2025-08-21 09:10:44 -0300

    estrutura do instrucoes

--- src/input/Instrucoes.java
+++ src/input/Instrucoes.java
@@ -2,15 +2,22 @@ package input;
 
 
 public class Instrucoes {
-    private String intrucao;
+    private String instrucao;
     private String r1;
     private String r2;
     private String r3;
 
-    public Instrucoes(String intrucao, String r1, String r2, String r3) {
-        this.intrucao = intrucao;
-        this.r1 = r1;
-        this.r2 = r2;
-        this.r3 = r3;
+    public Instrucoes(String instrucao, String r1, String r2, String r3) {
+        switch(instrucao){
+                case("add"){
+                    
+                } case("sub") {
+
+                } case("sw") {
+
+                } case("lw") {
+                } default() {
+                    System.out.println("Erro na criação da instrução")
+                }
     }
 }
